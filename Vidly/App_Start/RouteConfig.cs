4512fb62e83b2629dbfbc072626dfc4a7ebff7f8
jeantroiani <digitalguest@gmail.com>
2016-06-29 17:48:04 +0100

    Added custom routes

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -12,6 +12,15 @@ namespace Vidly
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            // new way of adding routes to your controllers.
+            routes.MapMvcAttributeRoutes();
+            // old way of doing it.
+            routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new { controller = "Movies", action = "ByReleaseDate"},
+                new { year = @"\d{4}", month = @"\d{2}" }
+            );
 
             routes.MapRoute(
                 name: "Default",
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -11,8 +11,18 @@ namespace Vidly.Controllers
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Frozen" };
+            var movie = new Movie { Name = "Frozen" };
             return View(movie);
+        }
+
+        public ActionResult ShowMovieData()
+        {
+            object movie = new Movie { Name = "Conan"};
+            //how to use View Data
+            ViewData["Movie"] = movie;
+            // how to use view bag
+            ViewBag.AnotherDisneyHit = "Beauty and the Beast";
+            return View();
         }
 
         //Action parameters
@@ -58,6 +68,18 @@ namespace Vidly.Controllers
         {
             return RedirectToAction("Index",new {pag= 12} );
         }
-        
+
+        // new routes created with a different structure than default.
+        public ActionResult ByReleaseDate(int? year, int month)
+        {
+            return Content($" looking for a movie released on {year} on the month of {month}");
+        }
+
+        [Route("movies/wishlist/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByWishListed(int? year, int month)
+        {
+            return Content($"The list of wishlisted movies added on this month {month} of this year {year}");
+        }
+
     }
 }
